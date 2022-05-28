0294a6765c87975ba41fed1d35f72413169f3c00
Felipe <suporte05@nbstelecom.com.br>
2022-05-28 15:38:17 -0400

    All changes to finish
    Finish the project.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCTtfDQKlcZuWi7b8XEBM5fFznU2fUGC5A",
+    authDomain: "reciclando-firebase.firebaseapp.com",
+    projectId: "reciclando-firebase",
+    storageBucket: "reciclando-firebase.appspot.com",
+    messagingSenderId: "696397774918",
+    appId: "1:696397774918:web:724cdca1f96bba275b4a47"
+  }
 };
 
 /*
